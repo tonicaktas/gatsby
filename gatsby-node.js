dcc2c21a8f583495dba8564036d44f2607246dfb
toni caktas <tonicaktas@tonis-iMac.lan>
2019-03-29 23:31:09 +0100

    seven.6

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const _ = require(`lodash`)
 const Promise = require(`bluebird`)
 const path = require(`path`)
 const slash = require(`slash`)
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 // Implement the Gatsby API “createPages”. This is
 // called after the Gatsby bootstrap is finished so you have
 // access to any information necessary to programmatically
