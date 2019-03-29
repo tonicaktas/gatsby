f249c5d3ff4f57139a8fd7616c87bdcee16e2b55
toni caktas <tonicaktas@tonis-iMac.lan>
2019-03-29 23:22:23 +0100

    seven.4

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,10 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: process.env.API_URL,
+        baseUrl: "webbcom.nu",
+        // baseUrl: process.env.API_URL,
         // The protocol. This can be http or https.
+        protocol: "https",
         protocol: process.env.API_PROTOCOL,
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
